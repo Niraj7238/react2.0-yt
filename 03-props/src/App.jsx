31c669f5160fbaad5d060f6ee6fc3486bac2268c
Niraj <mrskyd84@gmail.com>
2026-08-07 22:46:29 +0530

    props done

---
+++ 03-props/src/App.jsx
@@ -0,0 +1,22 @@
+import Card from './components/Card'
+
+const App = () => {
+  return (
+    <div className="parent">
+      <Card user='Niru' age={20} img="https://images.unsplash.com/photo-1773332585698-cba3c91b73e4?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyOXx8fGVufDB8fHx8fA%3D%3D"/>
+      <Card user='Alok' age={30} img="https://imgs.search.brave.com/_6izdbSVpHbwqbawMBquENAqLf6WtvwCZmTG8AhitpY/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4t/ZnJvbnQuZnJlZXBp/ay5jb20vaG9tZS9h/bm9uLXJ2bXAvY3Jl/YXRpdmUtc3VpdGUv/cGhvdG9ncmFwaHkv/Y2hhbmdlLWxvY2F0/aW9uLndlYnA"/>
+      <Card user='Nikhi' age={21} img="https://plus.unsplash.com/premium_photo-1785061834209-1e5d6cd779cc?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMXx8fGVufDB8fHx8fA%3D%3D" />
+      <Card user='Abhinay' age={20} img="https://plus.unsplash.com/premium_photo-1785031585896-ba9f62b98ee7?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxNnx8fGVufDB8fHx8fA%3D%3D"/>
+      <Card user='Rahul' age={30} img="https://images.unsplash.com/photo-1778674556730-357e85ec6cb2?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxOXx8fGVufDB8fHx8fA%3D%3D"/>
+      <Card user='Ankita' age={21} img="https://images.unsplash.com/photo-1773332611573-5e5bfa8e5de5?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyMnx8fGVufDB8fHx8fA%3D%3D" />
+      <Card user='Sanam' age={20} img="https://imgs.search.brave.com/iBF1u-6P5M1IlxObN804c3KihUaSoFzOOdeUf_s0WYU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pMC53/cC5jb20vcGljanVt/Ym8uY29tL3dwLWNv/bnRlbnQvdXBsb2Fk/cy9mYXNoaW9uLXBv/cnRyYWl0LW9mLXlv/dW5nLWJsYWNrLW1h/bi13aXRoLWdsYXNz/ZXMtYmx1ZS1tb29k/LWZyZWUtaW1hZ2Uu/anBlZz9oPTgwMCZx/dWFsaXR5PTgw"/>
+      <Card user='dolly' age={30} img="https://images.unsplash.com/photo-1779896412192-cca060dfafde?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzNnx8fGVufDB8fHx8fA%3D%3D"/>
+      <Card user='Samay' age={21} img="https://plus.unsplash.com/premium_photo-1785751093104-9b1900fc37a3?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1M3x8fGVufDB8fHx8fA%3D%3D" />
+      <Card user='Radha' age={20} img="https://imgs.search.brave.com/RdFGrjTAB55T3_jJAALjOUu1tmQRikPOfBFRSdl1zik/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9zdGFy/c3VuZm9sZGVkLmNv/bS93cC1jb250ZW50/L3VwbG9hZHMvMjAy/Mi8wNy9SYWRoYS1Z/YWRhdi1pbWFnZS5q/cGc"/>
+      <Card user='Somya' age={30} img="https://images.unsplash.com/photo-1779896412109-1b221c2b350b?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2NHx8fGVufDB8fHx8fA%3D%3D"/>
+      <Card user='Sanjana' age={21} img="https://images.unsplash.com/photo-1785845333757-cbbd4fc224af?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1NXx8fGVufDB8fHx8fA%3D%3D" />
+    </div>
+  )
+}
+
+export default App
