2e9fb522b0e1196d5057670e7e2b200943f336a9
Niraj <mrskyd84@gmail.com>
2026-08-07 22:42:40 +0530

    components done

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,17 @@
+import Card from './component/Card.jsx'
+import Navbar from './Navbar.jsx'
+
+const App = () => {
+
+  const user = 'NIRAJ KUMAR YADAV';
+  const age = 20;
+  return (
+    <div>
+      <Navbar />
+      <Card />
+      
+    </div>
+  )
+}
+
+export default App
---
+++ 02-components/src/component/Card.jsx
@@ -0,0 +1,8 @@
+function Card(){
+
+    return <div className="card">
+        <h1>Niraj kumar Yadav</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At adipisci eum eos eius ducimus minus ipsa debitis sunt numquam nulla rem modi corporis quasi blanditiis, maiores commodi, delectus aliquid! Hic.</p>
+    </div>
+}
+export default Card;
