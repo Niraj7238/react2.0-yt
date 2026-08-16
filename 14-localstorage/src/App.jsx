52af57ca10f1b2d63474b875de2edb44a90e8247
Niraj <mrskyd84@gmail.com>
2026-08-16 23:11:16 +0530

    initial commit

---
+++ 14-localstorage/src/App.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const App = () => {
+  
+
+  //localStorage.setItem('user','niraj')
+  // const user = localStorage.getItem('user')
+  // console.log(user);
+
+
+  //  localStorage.setItem('age','20');
+  // const age = localStorage.getItem('age')
+  // console.log(age);
+  
+
+
+  // localStorage.clear();
+
+
+    // const user = {
+    //   usernam: 'niraj',
+    //   age :18,
+    //   city:'deoria'
+    // }
+    // localStorage.setItem('user',JSON.stringify(user));
+
+
+
+    // const user = JSON.parse(localStorage.getItem('user'))
+    // console.log(user);
+
+
+
+
+  return (
+    <div>
+      App
+    </div>
+  )
+}
+
+export default App
